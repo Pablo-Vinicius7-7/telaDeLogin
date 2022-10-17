3b964f1d7bf9b372222acd907499c42621bf237a
Pablo-Vinicius7-7 <pablovinicius1703@gmail.com>
2022-10-17 14:56:20 -0300

    modificações da aula do dia 17/10

--- app/src/main/java/com/example/teladelogin/CadastroActivity.java
+++ app/src/main/java/com/example/teladelogin/CadastroActivity.java
@@ -10,5 +10,6 @@ public class CadastroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
     }
 }
